d50579a1fb82cdd3d648e39a004fb47751ede313
Eduardo <eduardooliveira7907@gmail.com>
2026-04-28 14:37:52 -0300

    implementando validacao de formulario e exibicao

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,27 @@ if(currencyBar) {
     const cotacoes =  await buscarCotacoes();
 
     if(cotacoes){
-        currencyBar.innerHTML = `<span><strong>USD:</strong> R$ ${cotacoes}</sapn> | <span><strong>EUR:</strong> R$ ${cotacoes.euro}</span>`;
+        currencyBar.innerHTML = `<span><strong>USD:</strong> R$ ${cotacoes.dolar}</span> | <span><strong>EUR:</strong> R$ ${cotacoes.euro}</span>`;
     }
 }
+const formAdquirir = document.getElementById('form-adquirir');
+if(formAdquirir) {
+    formAdquirir.addEventListener('submit' (evento)  => {
+        evento.preventDefault(); 
+        const nome = document.getElementById('campo-nome').ariaValueMax.trim();
+        const email = document.getElementById('campo-email').value.trim();
+        const telefone = document.getElementById('campo-telefone').value.trim();
 
-})
+        if (!nome|| !email || !telefone ) {
+            alert("Carlos ventura precisa do seu nome, e-mail e telefone para retornar o contato!");
+        } else {
+            alert("Solicitação enviada com sucesso! O artista entrará em contato.");
+            formAdquirir.reset();
+        }
+    });
+}
+
+    /// Parte do João Marcos
+    // João, insire aqui a lógica de esconder/mostrar as obras
+
+});
