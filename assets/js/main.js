2270504abb2634a25dbaf0b79081baa12601b399
Eduardo <eduardooliveira7907@gmail.com>
2026-04-28 15:39:20 -0300

    implementando conversao dinâmica de preços na galeria

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,21 +8,25 @@ if(currencyBar) {
         currencyBar.innerHTML = `<span><strong>USD:</strong> R$ ${cotacoes.dolar}</span> | <span><strong>EUR:</strong> R$ ${cotacoes.euro}</span>`;
     }
 }
+
 const formAdquirir = document.getElementById('form-adquirir');
 if(formAdquirir) {
-    formAdquirir.addEventListener('submit' (evento)  => {
+    formAdquirir.addEventListener('submit', (evento)  => {
         evento.preventDefault(); 
-        const nome = document.getElementById('campo-nome').ariaValueMax.trim();
+
+        const nome = document.getElementById('campo-nome').Value.trim();
         const email = document.getElementById('campo-email').value.trim();
         const telefone = document.getElementById('campo-telefone').value.trim();
 
-        if (!nome|| !email || !telefone ) {
+        if (!nome || !email || !telefone) {
             alert("Carlos ventura precisa do seu nome, e-mail e telefone para retornar o contato!");
         } else {
+
             alert("Solicitação enviada com sucesso! O artista entrará em contato.");
             formAdquirir.reset();
         }
     });
+
 }
 
     /// Parte do João Marcos
